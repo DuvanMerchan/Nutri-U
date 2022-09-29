ff7d6851160bad8e3c5daf1b54b5be09ea2d1b6d
Duván Merchán <damb.dark.95@gmail.com>
2022-09-28 21:38:39 -0500

    redux commit

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import recipes from '../redux/recipeSlice';
 
 
 export const store = configureStore({
   reducer: {
+    recipes: recipes,
+    // users:users,
   },
 });
