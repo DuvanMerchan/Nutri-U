548f97683c166ab7b46a688b572068990e4abe99
Duván Merchán <damb.dark.95@gmail.com>
2022-09-29 16:02:54 -0500

    receta

---
+++ client/src/components/receta.js
@@ -0,0 +1,12 @@
+export const receta1 ={
+    id: 716426,
+    title: "Cauliflower, Brown Rice, and Vegetable Fried Rice",
+    image: "https://spoonacular.com/recipeImages/716426-312x231.jpg",
+    healthScore: 76,
+    diets: [
+        "gluten free",
+        "dairy free",
+        "lacto ovo vegetarian",
+        "vegan"
+            ],
+}
