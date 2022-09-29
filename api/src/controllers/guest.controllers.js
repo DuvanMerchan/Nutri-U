20bb932e628b77f547e4759ca7be9962c2a65492
Pedro Caloiero <caloieropedro@gmail.com>
2022-09-29 15:39:27 -0300

    Ruta Detalle del Plato

---
+++ api/src/controllers/guest.controllers.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const axios = require('axios');
+const { API_KEY } = process.env;
+
+const recipeDetail = async (req, res) =>{
+    
+    const { recipeId } = req.params;
+    
+    try{
+        let { data } = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/information?&apiKey=${API_KEY}`)
+        if(data.hasOwnProperty('id')){
+            let dataMapped = await data?.map(e =>{
+                return{
+                    id: e.id,
+                    name: e.title,
+                    image: e.image,
+                    summary: e.summary,
+                    diets: e.diets,
+                    vegetarian: e.vegetarian,
+                    vegan: e.vegan,
+                    glutenFree: e.glutenFree,
+                    dairyFree: e. dairyFree,
+                    veryHealthy: e.veryHealthy,
+                    instructions: e.instructions
+                }
+            })
+            res.json(dataMapped)
+        } else {
+            throw new Error(`We cant find a recipe with id: ${recipeId}`)
+        }
+    }catch(e){
+        res.send(e.message)
+    }
+}
+
+module.exports = recipeDetail;
