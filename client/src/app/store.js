097c99b06b54afd7040913693d55c280644bb7b5
Duván Merchán <damb.dark.95@gmail.com>
2022-09-29 12:30:56 -0500

    ingredientsactions

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import ingredients from '../redux/ingredientSlice';
 import recipes from '../redux/recipeSlice';
 
 
 export const store = configureStore({
   reducer: {
     recipes: recipes,
+
+    ingredients: ingredients
     // users:users,
   },
 });
